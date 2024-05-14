680606a839e23e489d547e215e70e53ce40dec7a
Мария Рудометова <rudometovamo@student.bmstu.ru>
2024-05-15 00:40:52 +0300

    create pages on deploy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { fileURLToPath, URL } from "node:url";
-
+import envCompatible from "vite-plugin-env-compatible";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     base: "/todo-list",
-    plugins: [vue()],
+    envPrefix: "VUE_APP_",
+    plugins: [vue(), envCompatible()],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),

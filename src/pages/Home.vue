7f90752f8485cd4eec05fe1a953416b358b49c1d
Мария Рудометова <rudometovamo@student.bmstu.ru>
2024-07-01 20:21:13 +0300

    Updates

--- src/pages/Home.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-        <h2>Домашняя страница</h2>
-        <p>Для просмотра списка дел перейдите по ссылке ниже</p>
-        <router-link to="/todos">Список дел</router-link>
-    </div>
-</template>
